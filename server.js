7f8ec25a52a0e295983742b2d3fefad49efb318a
Eric <ericnguyen23@gmail.com>
2023-04-03 16:49:45 -0700

    Added sign up route

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 // const helpers = require("./utils/helpers");
 
-// const sequelize = require("./config/connection");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,9 +25,9 @@ const sess = {
   },
   resave: false,
   saveUninitialized: true,
-  // store: new SequelizeStore({
-  //   db: sequelize,
-  // }),
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -41,6 +41,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () => console.log("Now listening"));
-// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
