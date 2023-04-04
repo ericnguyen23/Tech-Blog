e937ff74508d5c4570720c9206219cb4f5e92e98
Eric <ericnguyen23@gmail.com>
2023-04-03 21:11:41 -0700

    Added comment js and Model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const User = require("./User");
 const BlogPost = require("./BlogPost");
+const Comment = require("./Comment");
 
 User.hasMany(BlogPost, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
 });
@@ -10,4 +16,8 @@ BlogPost.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, BlogPost };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, BlogPost, Comment };
