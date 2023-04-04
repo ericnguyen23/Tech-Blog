a68048047641c376051029cd3faf1c5755ecafc3
Eric <ericnguyen23@gmail.com>
2023-04-03 18:15:36 -0700

    Added logged in state

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const { Project, User } = require("../models");
+const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
 // home route
@@ -73,16 +73,6 @@ router.get("/", async (req, res) => {
 //   }
 // });
 
-router.get("/login", (req, res) => {
-  // // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect("/profile");
-  //   return;
-  // }
-
-  res.render("login");
-});
-
 router.get("/signup", (req, res) => {
   // // If the user is already logged in, redirect the request to another route
   // if (req.session.logged_in) {
@@ -103,14 +93,34 @@ router.get("/dashboard", (req, res) => {
   res.render("dashboard");
 });
 
-router.get("/profile", (req, res) => {
-  // // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect("/profile");
-  //   return;
-  // }
+// Use withAuth middleware to prevent access to route
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      // include: [{ model: Project }],
+    });
 
-  res.render("profile");
+    const user = userData.get({ plain: true });
+    // render user information in handlebar's profile tempalte
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
