f10f0083212a971b3ee9b27064f9ee0c50029f18
Eric <ericnguyen23@gmail.com>
2023-04-03 18:39:41 -0700

    added redirect if logged in within sign up page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,11 +74,10 @@ router.get("/", async (req, res) => {
 // });
 
 router.get("/signup", (req, res) => {
-  // // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect("/profile");
-  //   return;
-  // }
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
 
   res.render("signup");
 });
